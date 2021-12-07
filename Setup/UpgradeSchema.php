8e966bbd351ca0f440913c2aefa85574a1fbdf6e
dbashyal <dbashyal@gmail.com>
2021-12-07 16:42:50 +1100

    fix missing tablename in addIndex

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -243,10 +243,10 @@ class UpgradeSchema implements UpgradeSchemaInterface
         }
 
         if (version_compare($context->getVersion(), '1.2.4.1', '<')) {
+            $tableName = $setup->getTable('mageplaza_smtp_log');
             $connection->addIndex(
-                $setup->getIdxName(
-                    $setup->getTable('mageplaza_smtp_log'),
-                    ['recipient']),
+                $tableName,
+                $setup->getIdxName($tableName, ['recipient']),
                 ['recipient']
             );
         }
